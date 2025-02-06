333345f48d6f10319055df17bd5b66429e419db0
Amirbek_Amonov <amonovamirbek1@gmail.com>
2025-02-06 17:19:06 +0500

    AOS qoshildi

--- src/components/sections/Hero/hero.jsx
+++ src/components/sections/Hero/hero.jsx
@@ -1,14 +1,25 @@
+import React, { useEffect} from 'react'
 import './hero.scss'
 import Input from './input/input'
+import Aos from 'aos';
+import "aos/dist/aos.css";
+
 
 function Hero() {
+    useEffect(() => {
+        Aos.init({ 
+            duration: 800,
+            easing: "ease-in-out", 
+            once: true, 
+        });
+    }, [])
     return (
         <section className='hero'>
             <div className='container'>
                 <div className='hero__content'>
-                    <h1>Awesome tools for
+                    <h1 data-aos="fade-up">Awesome tools for
                         Designer & Developer <span>.</span></h1>
-                    <p>Antool is a web collection of information on paid or free Design and Development tools</p>
+                    <p data-aos="fade-up" data-aos-delay="200">Antool is a web collection of information on paid or free Design and Development tools</p>
                     <Input />
                     <div className='hero__content__img'>
                         <a href="https://www.facebook.com"><img src="./assets/icons/facebook.svg" alt="" /></a>
--- src/components/sections/Hero/input/input.jsx
+++ src/components/sections/Hero/input/input.jsx
@@ -2,7 +2,7 @@ import './input.scss'
 
 function Input() {
     return (
-       <label htmlFor="" class="label">
+       <label htmlFor="" class="label" data-aos="fade-up" data-aos-delay="400">
          <div class="input-container">
             <img src="./assets/icons/search.svg" alt="" />
             <input type="text" placeholder='find more than 430+ tools...' />
