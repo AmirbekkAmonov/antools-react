cdc761faca7a78ee2ef3d2739906bd0aae0ae500
Amirbek_Amonov <amonovamirbek1@gmail.com>
2025-02-06 19:58:26 +0500

    mobile, aos qoshildi

--- src/components/sections/Brand/brand.jsx
+++ src/components/sections/Brand/brand.jsx
@@ -2,15 +2,15 @@ import './brand.scss'
 
 function Brand() {
     return (
-        <section className='brand'>
-            <div className='container'>
+        <section className='brand' >
+            <div className='container' data-aos="fade-up">
                 <div className='brand__content'>
-                <b>Trusted more than 150+ brand</b>
+                <b data-aos="fade-zoom-in">Trusted more than 150+ brand</b>
                 <div className='brand__img'>
-                    <img src="./assets/images/logos_microsoft.png" alt="" />
-                    <img src="./assets/images/logos_google.png" alt="" />
-                    <img src="./assets/images/logos_slack.png" alt="" />
-                    <img src="./assets/images/logos_wordpress.png" alt="" />
+                    <img src="./assets/images/logos_microsoft.png" alt="" data-aos="fade-up" />
+                    <img src="./assets/images/logos_google.png" alt="" data-aos="fade-up" data-aos-delay="100" />
+                    <img src="./assets/images/logos_slack.png" alt="" data-aos="fade-up" data-aos-delay="200" />
+                    <img src="./assets/images/logos_wordpress.png" alt="" data-aos="fade-up" data-aos-delay="300" />
                 </div>
                 </div>
             </div>
